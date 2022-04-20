renesas-rz/rz-edge-ai-demo

commit 820846c5c736350584e7d5b727b2c2dfbef62195

command-line: Add starting mode option
This allows the user to select the mode which the application starts in
from the command line.

Signed-off-by: Gareth Williams <gareth.williams.jx@renesas.com>

mainwindow.cpp
@@ -35,12 +35,13 @@
 #include "videoworker.h"
 #include "shoppingbasket.h"
 
-MainWindow::MainWindow(QWidget *parent, QString cameraLocation, QString labelLocation, QString modelLocation)
+MainWindow::MainWindow(QWidget *parent, QString cameraLocation, QString labelLocation, QString modelLocation, Mode mode)
     : QMainWindow(parent),
       ui(new Ui::MainWindow)
 {
     Board board = Unknown;
     inputMode = cameraMode;
+    demoMode = mode;
 
     QPixmap splashScreenImage(SPLASH_SCREEN_DIRECTORY);
 
@@ -146,7 +147,11 @@ MainWindow::MainWindow(QWidget *parent, QString cameraLocation, QString labelLoc
     } else {
         createVideoWorker();
         createTfWorker();
-        setupObjectDetectMode();
+
+        if (demoMode == SB)
+            setupShoppingMode();
+        else if (demoMode == OD)
+            setupObjectDetectMode();
 
         /* Limit camera loop speed if using mipi camera to save on CPU
          * USB camera is alreay limited to 10 FPS */
@@ -177,6 +182,7 @@ void MainWindow::setupObjectDetectMode()
 void MainWindow::setupShoppingMode()
 {
     demoMode = SB;
+    modelPath = MODEL_DIRECTORY_SB;
 
     shoppingBasketMode = new shoppingBasket(ui);
 
@@ -425,7 +431,6 @@ void MainWindow::on_actionShopping_Basket_triggered()
     modelObjectDetect = modelPath;
 
     inputMode = cameraMode;
-    modelPath = MODEL_DIRECTORY_SB;
 
     if (cvWorker->getUsingMipi())
         iterations = 6;

main.cpp
@@ -29,9 +29,13 @@ int main(int argc, char *argv[])
     QCommandLineOption cameraOption(QStringList() << "c" << "camera", "Choose a camera.", "file");
     QCommandLineOption labelOption (QStringList() << "l" << "label", "Choose a label for Object Detection Mode.", "file", QString(LABEL_DIRECTORY_OD));
     QCommandLineOption modelOption (QStringList() << "m" << "model", "Choose a model for Object Detection Mode.", "file", QString(MODEL_DIRECTORY_OD));
+    QCommandLineOption modeOption (QStringList() << "s" << "start-mode",
+                                   "Choose a mode to start the application in: [shopping-basket|object-detection].", "mode", QString("object-detection"));
     QString cameraLocation;
     QString labelLocation;
     QString modelLocation;
+    QString modeString;
+    Mode mode = OD;
     QString applicationDescription =
     "Selecting Demo Mode\n"
     "Demo Mode->Object Detection: Object Detection Mode.\n"
@@ -87,15 +91,24 @@ int main(int argc, char *argv[])
     parser.addOption(cameraOption);
     parser.addOption(labelOption);
     parser.addOption(modelOption);
+    parser.addOption(modeOption);
     parser.addHelpOption();
     parser.setApplicationDescription(applicationDescription);
     parser.process(a);
     cameraLocation = parser.value(cameraOption);
     labelLocation = parser.value(labelOption);
     modelLocation = parser.value(modelOption);
+    modeString = parser.value(modeOption);
+
+    if (modeString == "shopping-basket")
+        mode = SB;
+    else if (modeString == "object-detection")
+        mode = OD;
+    else
+        qWarning("Warning: unknown demo mode requested, starting in default mode...");
 
     QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-    MainWindow w(nullptr, cameraLocation, labelLocation, modelLocation);
+    MainWindow w(nullptr, cameraLocation, labelLocation, modelLocation, mode);
     w.show();
     return a.exec();
 }

mainwindow.h
@@ -79,7 +79,7 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent, QString cameraLocation, QString labelLocation, QString modelLocation);
+    MainWindow(QWidget *parent, QString cameraLocation, QString labelLocation, QString modelLocation, Mode mode);
 
 public slots:
     void ShowVideo();

commit a3c891ca42f71e4aff57c84b346a469cb567a6fb

models: Ensure correct model selection when changing modes by GUI
Changing modes with the GUI can result in the model path for the
Shopping Basket mode not being set correctly. Restructuring when
this assignment is made fixes this. We should also save the model
path provided by the user for the Object Detection mode in case
the app is started in Shopping Basket mode.

Fixes: 820846c5c736

Signed-off-by: Gareth Williams <gareth.williams.jx@renesas.com>

mainwindow.cpp
@@ -59,7 +59,13 @@ MainWindow::MainWindow(QWidget *parent, QString cameraLocation, QString labelLoc
     if (labelPath.isEmpty())
         qWarning("Warning: Label file path not provided");
 
-    modelPath = modelLocation;
+    modelObjectDetect = modelLocation;
+
+    if (demoMode == SB)
+        modelPath = MODEL_DIRECTORY_SB;
+    else
+        modelPath = modelLocation;
+
     if (modelPath.isEmpty())
           qWarning("Warning: Model file path not provided");
 
@@ -184,7 +190,6 @@ void MainWindow::setupObjectDetectMode()
 void MainWindow::setupShoppingMode()
 {
     demoMode = SB;
-    modelPath = MODEL_DIRECTORY_SB;
 
     shoppingBasketMode = new shoppingBasket(ui, pricesPath);
 
@@ -433,6 +438,7 @@ void MainWindow::on_actionShopping_Basket_triggered()
     modelObjectDetect = modelPath;
 
     inputMode = cameraMode;
+    modelPath = MODEL_DIRECTORY_SB;
 
     if (cvWorker->getUsingMipi())
         iterations = 6;