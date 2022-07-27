renesas-rz/rz-edge-ai-demo

commit d38698a050f33342bff3022179f1a617523289cc

Improve code consistency across demo modes
Signed-off-by: Ayman Chaudhry <ayman.chaudhry.kc@renesas.com>

poseestimation.cpp
@@ -60,6 +60,9 @@ poseEstimation::poseEstimation(Ui::MainWindow *ui, PoseModel poseModel)
 
     utilPE = new edgeUtils();
 
+    frameHeight = GRAPHICS_VIEW_HEIGHT;
+    frameWidth = GRAPHICS_VIEW_WIDTH;
+
     uiPE->actionShopping_Basket->setDisabled(false);
     uiPE->actionObject_Detection->setDisabled(false);
     uiPE->actionPose_Estimation->setDisabled(true);
@@ -80,12 +83,12 @@ poseEstimation::poseEstimation(Ui::MainWindow *ui, PoseModel poseModel)
 
 void poseEstimation::setButtonState(bool enable)
 {
-    if (enable) {
-        buttonState = true;
+    buttonState = enable;
+
+    if (buttonState) {
         uiPE->pushButtonStartStopPose->setText("Start\nInference");
         uiPE->pushButtonStartStopPose->setStyleSheet(BUTTON_BLUE);
     } else {
-        buttonState = false;
         uiPE->pushButtonStartStopPose->setText("Stop\nInference");
         uiPE->pushButtonStartStopPose->setStyleSheet(BUTTON_RED);
     }
@@ -403,6 +406,8 @@ void poseEstimation::drawLimbsHandPose(const QVector<float> &outputTensor, bool 
 void poseEstimation::connectLimbs(int limb1, int limb2, bool drawGraphicalViewLimbs)
 {
     QPen pen;
+    bool nanLimb1;
+    bool nanLimb2;
 
     if (poseModelSet == HandPose) {
         pen.setWidth(PEN_WIDTH_HAND_POSE);
@@ -412,13 +417,16 @@ void poseEstimation::connectLimbs(int limb1, int limb2, bool drawGraphicalViewLi
         pen.setColor(LINE_RED);
     }
 
-    if (std::isnan(xCoordinate[limb1]) == false && std::isnan(yCoordinate[limb1]) == false) {
-        if (std::isnan(xCoordinate[limb2]) == false && std::isnan(yCoordinate[limb2]) == false) {
-                    if (drawGraphicalViewLimbs)
-                    uiPE->graphicsViewPointProjection->scene()->addLine(xCoordinate[limb1], yCoordinate[limb1], xCoordinate[limb2], yCoordinate[limb2], pen);
-                    else
-            		uiPE->graphicsView->scene()->addLine(xCoordinate[limb1], yCoordinate[limb1], xCoordinate[limb2], yCoordinate[limb2], pen);
-    	}
+    nanLimb1 = std::isnan(xCoordinate[limb1]) && std::isnan(yCoordinate[limb1]);
+    nanLimb2 = std::isnan(xCoordinate[limb2]) && std::isnan(yCoordinate[limb2]);
+
+    if (!nanLimb1 || !nanLimb2) {
+        QLine *lineToDraw = new QLine(xCoordinate[limb1], yCoordinate[limb1], xCoordinate[limb2], yCoordinate[limb2]);
+
+        if (drawGraphicalViewLimbs)
+            uiPE->graphicsViewPointProjection->scene()->addLine(*lineToDraw, pen);
+        else
+            uiPE->graphicsView->scene()->addLine(*lineToDraw, pen);
     }
 }
 

objectdetection.cpp
@@ -67,12 +67,12 @@ objectDetection::objectDetection(Ui::MainWindow *ui, QStringList labelFileList)
 
 void objectDetection::setButtonState(bool enable)
 {
-    if (enable) {
-        buttonState = true;
+    buttonState = enable;
+
+    if (buttonState) {
         uiOD->pushButtonStartStop->setText("Start\nInference");
         uiOD->pushButtonStartStop->setStyleSheet(BUTTON_BLUE);
     } else {
-        buttonState = false;
         uiOD->pushButtonStartStop->setText("Stop\nInference");
         uiOD->pushButtonStartStop->setStyleSheet(BUTTON_RED);
     }

commit 16d8a492ce8fba71c69103acdce8689ae3e37c47

pose-estimation: Correct limb connection logic
The logic that checks for NaN values during limb connection currently
always resolves to true which means there is not a filter for values
that cannot be displayed. We should correct this logic to ensure the
displayed values are correct.

Fixes: d38698a050f3

Signed-off-by: Gareth Williams <gareth.williams.jx@renesas.com>

poseestimation.cpp
@@ -425,10 +425,10 @@ void poseEstimation::connectLimbs(int limb1, int limb2, bool drawGraphicalViewLi
         pen.setColor(LINE_RED);
     }
 
-    nanLimb1 = std::isnan(xCoordinate[limb1]) && std::isnan(yCoordinate[limb1]);
-    nanLimb2 = std::isnan(xCoordinate[limb2]) && std::isnan(yCoordinate[limb2]);
+    nanLimb1 = !std::isnan(xCoordinate[limb1]) || !std::isnan(yCoordinate[limb1]);
+    nanLimb2 = !std::isnan(xCoordinate[limb2]) || !std::isnan(yCoordinate[limb2]);
 
-    if (!nanLimb1 || !nanLimb2) {
+    if (nanLimb1 && nanLimb2) {
         QLine *lineToDraw = new QLine(xCoordinate[limb1], yCoordinate[limb1], xCoordinate[limb2], yCoordinate[limb2]);
 
         if (drawGraphicalViewLimbs)