renesas-rz/rz-edge-ai-demo

commit e39d692039264ca930ada37d6930088c10ceb5cb

mainwindow: Use media sub directories for each mode
Example media is distributed in directories in the RFS for each
mode. We should open the file menu in the correct directory for
the running mode for the user.

Signed-off-by: Gareth Williams <gareth.williams.jx@renesas.com>

mainwindow.h
@@ -38,6 +38,10 @@
 #define LABEL_PATH_OD "/opt/rz-edge-ai-demo/labels/mobilenet_ssd_v2_coco_quant_postprocess_labels.txt"
 #define LABEL_PATH_SB "/opt/rz-edge-ai-demo/labels/shoppingBasketDemo_labels.txt"
 #define MEDIA_DIRECTORY "/opt/rz-edge-ai-demo/media/"
+#define MEDIA_DIRECTORY_SB "/opt/rz-edge-ai-demo/media/shopping-basket/"
+#define MEDIA_DIRECTORY_FD "/opt/rz-edge-ai-demo/media/face-detection/"
+#define MEDIA_DIRECTORY_PE "/opt/rz-edge-ai-demo/media/pose-estimation/"
+#define MEDIA_DIRECTORY_OD "/opt/rz-edge-ai-demo/media/object-detection/"
 #define MODEL_DIRECTORY "/opt/rz-edge-ai-demo/models/"
 #define MODEL_PATH_OD "/opt/rz-edge-ai-demo/models/mobilenet_ssd_v2_coco_quant_postprocess.tflite"
 #define MODEL_PATH_SB "/opt/rz-edge-ai-demo/models/shoppingBasketDemo.tflite"

mainwindow.cpp
@@ -1009,6 +1009,7 @@ void MainWindow::on_actionLoad_File_triggered()
     QFileDialog dialog(this);
     QString mediaFileFilter;
     QString mediaFileName;
+    QString mediaDir;
 
     connect(this, SIGNAL(fileLoaded()), qeventLoop, SLOT(quit()));
 
@@ -1020,7 +1021,20 @@ void MainWindow::on_actionLoad_File_triggered()
 
     dialog.setFileMode(QFileDialog::AnyFile);
     dialog.setViewMode(QFileDialog::Detail);
-    dialog.setDirectory(MEDIA_DIRECTORY);
+
+    switch (demoMode)
+    {
+       case SB: mediaDir = MEDIA_DIRECTORY_SB;
+         break;
+       case OD: mediaDir = MEDIA_DIRECTORY_FD;
+         break;
+       case PE: mediaDir = MEDIA_DIRECTORY_PE;
+         break;
+       case FD: mediaDir = MEDIA_DIRECTORY_OD;
+         break;
+       default: mediaDir = MEDIA_DIRECTORY;
+    }
+    dialog.setDirectory(mediaDir);
 
     if (demoMode != SB)
         mediaFileFilter += VIDEO_FILE_FILTER;

commit 1faf14c29e0a02d27c44b0ae94043cf7bee2148c

video: Update default video filepaths
Fixes: e39d69203926

Signed-off-by: Gareth Williams <gareth.williams.jx@renesas.com>

mainwindow.h
@@ -56,9 +56,9 @@
 #define MODEL_PATH_PE_HAND_POSE_FULL "/opt/rz-edge-ai-demo/models/hand_landmark_full.tflite"
 #define RENESAS_RZ_LOGO_PATH "/opt/rz-edge-ai-demo/logos/renesas-rz-logo.png"
 #define SPLASH_SCREEN_PATH "/opt/rz-edge-ai-demo/logos/rz-splashscreen.png"
-#define DEFAULT_VIDEO "/opt/rz-edge-ai-demo/media/exercising_using_battle_ropes.mp4"
-#define DEFAULT_FD_VIDEO "/opt/rz-edge-ai-demo/media/face_shaking.mp4"
-#define DEFAULT_SBD_IMG "/opt/rz-edge-ai-demo/media/shopping_items_003.jpg"
+#define DEFAULT_VIDEO "/opt/rz-edge-ai-demo/media/pose-estimation/exercising_using_battle_ropes.mp4"
+#define DEFAULT_FD_VIDEO "/opt/rz-edge-ai-demo/media/face-detection/face_shaking.mp4"
+#define DEFAULT_SBD_IMG "/opt/rz-edge-ai-demo/media/shopping-basket/shopping_items_003.jpg"
 
 #define OPTION_FD_DETECT_FACE "face"
 #define OPTION_FD_DETECT_IRIS "iris"