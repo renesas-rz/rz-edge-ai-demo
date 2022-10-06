renesas-rz/rz-edge-ai-demo

commit 8fd6e2eb5656317c022ea124424ea401b4996812

Use meta-renesas-ai v5.0.0
meta-renesas-ai v5.0.0 is based on RZ/G2 BSP v3.0.0. This allows all
platforms to use the same versions of tensorflow-lite and ArmNN,
expanding the inference options available to the RZ/G2E, RZ/G2M and
RZ/G2H. Using the same framework versions for all platforms also
removes the need for the qmake DUNFELL define previously used for
the RZ/G2L and RZ/G2LC.

Signed-off-by: Gareth Williams <gareth.williams.jx@renesas.com>

mainwindow.cpp
@@ -87,10 +87,7 @@ MainWindow::MainWindow(QWidget *parent, QString boardName, QString cameraLocatio
 
     ui->actionEnable_ArmNN_Delegate->setEnabled(false);
     ui->actionTensorFlow_Lite->setEnabled(true);
-
-#ifndef DUNFELL
     ui->actionTensorflow_Lite_XNNPack_delegate->setEnabled(false);
-#endif
 
     connect(this, SIGNAL(sendMatToDraw(cv::Mat)), this, SLOT(drawMatToView(cv::Mat)));
 
@@ -617,9 +614,7 @@ void MainWindow::on_actionEnable_ArmNN_Delegate_triggered()
 
     ui->actionEnable_ArmNN_Delegate->setEnabled(false);
     ui->actionTensorFlow_Lite->setEnabled(true);
-#ifdef DUNFELL
     ui->actionTensorflow_Lite_XNNPack_delegate->setEnabled(true);
-#endif
 
     remakeTfWorker();
 }
@@ -659,9 +654,7 @@ void MainWindow::on_actionTensorFlow_Lite_triggered()
     }
 
     ui->actionTensorFlow_Lite->setEnabled(false);
-#ifdef DUNFELL
     ui->actionTensorflow_Lite_XNNPack_delegate->setEnabled(true);
-#endif
 
     remakeTfWorker();
 }

.gitlab-ci.yml
@@ -1,4 +1,5 @@
 .app-build:
+  image: gitlab.renesas.solutions:5050/spl2/continuous-integration/dockerfiles:ubuntu-20.04-latest
   stage: build
   variables:
     META_LAYER_REF: master
@@ -16,7 +17,7 @@
     - mkdir ${PWD}/toolchain
     - ./sdk.sh -y -d ${PWD}/toolchain
     - source ${PWD}/toolchain/environment-setup-aarch64-poky-linux
-    - qmake ${QDEFINES}
+    - qmake
     - make
   artifacts:
     name: "${CI_JOB_NAME}-${CI_JOB_ID}"
@@ -26,30 +27,24 @@
       - rz-edge-ai-demo
 
 hihope-rzg2m:
-  image: gitlab.renesas.solutions:5050/spl2/continuous-integration/dockerfiles:ubuntu-18.04-latest
   extends: .app-build
   variables:
     PLATFORM: hihope-rzg2m
 
 ek874:
-  image: gitlab.renesas.solutions:5050/spl2/continuous-integration/dockerfiles:ubuntu-18.04-latest
   extends: .app-build
   variables:
     PLATFORM: ek874
 
 smarc-rzg2l:
-  image: gitlab.renesas.solutions:5050/spl2/continuous-integration/dockerfiles:ubuntu-20.04-latest
   extends: .app-build
   variables:
     PLATFORM: smarc-rzg2l
-    QDEFINES: 'DEFINES+=DUNFELL'
 
 smarc-rzg2lc:
-  image: gitlab.renesas.solutions:5050/spl2/continuous-integration/dockerfiles:ubuntu-20.04-latest
   extends: .app-build
   variables:
     PLATFORM: smarc-rzg2lc
-    QDEFINES: 'DEFINES+=DUNFELL'
 
 trigger-bsp-build:
   stage: deploy

rz-edge-ai-demo.pro
@@ -62,10 +62,7 @@ LIBS += \
     -lopencv_videoio \
     -ltensorflow-lite \
     -ldl \
-    -lutil
-
-contains(DEFINES, DUNFELL) {
-LIBS += \
+    -lutil \
     -lflatbuffers \
     -lfft2d_fftsg2d \
     -lruy \
@@ -75,4 +72,3 @@ LIBS += \
     -lclog \
     -lfft2d_fftsg \
     -lfarmhash
-}

tfliteworker.cpp
@@ -26,9 +26,7 @@
 #include <armnn/Utils.hpp>
 #include <delegate/armnn_delegate.hpp>
 #include <delegate/DelegateOptions.hpp>
-#ifdef DUNFELL
 #include "tensorflow/lite/delegates/xnnpack/xnnpack_delegate.h"
-#endif
 
 tfliteWorker::tfliteWorker(QString modelLocation, Delegate delegateType, int defaultThreads)
 {
@@ -53,7 +51,6 @@ tfliteWorker::tfliteWorker(QString modelLocation, Delegate delegateType, int def
            qWarning("ArmNN Delegate could not be used to modify the graph\n");
     }
 
-#ifdef DUNFELL
     if (delegateType == xnnpack) {
         TfLiteXNNPackDelegateOptions xnnpack_options = TfLiteXNNPackDelegateOptionsDefault();
 
@@ -63,7 +60,6 @@ tfliteWorker::tfliteWorker(QString modelLocation, Delegate delegateType, int def
         if (tfliteInterpreter->ModifyGraphWithDelegate(xnnpack_delegate) != kTfLiteOk)
             qWarning("Could not modifiy Graph with XNNPack Delegate\n");
     }
-#endif
 
     if (tfliteInterpreter->AllocateTensors() != kTfLiteOk)
         qFatal("Failed to allocate tensors!");
@@ -80,10 +76,8 @@ tfliteWorker::tfliteWorker(QString modelLocation, Delegate delegateType, int def
 tfliteWorker::~tfliteWorker() {
     tfliteInterpreter.reset();
 
-#ifdef DUNFELL
     if (delegateType == xnnpack)
         TfLiteXNNPackDelegateDelete(xnnpack_delegate);
-#endif
 }
 
 /* Resize the input image and manipulate the data such that the alpha channel

tfliteworker.h
@@ -57,9 +57,7 @@ private:
     QString modelName;
     Delegate delegateType;
     Mode modeSelected;
-#ifdef DUNFELL
     TfLiteDelegate* xnnpack_delegate;
-#endif
     QVector<float> outputTensor;
     int wantedWidth, wantedHeight, wantedChannels;
 };

commit 52719bfce9175b40b5ac265380c3d140361a5705

ui: Enable XNNPack delegate menu item during start
Fixes: 8fd6e2eb5656

Signed-off-by: Gareth Williams <gareth.williams.jx@renesas.com>

mainwindow.cpp
@@ -87,7 +87,7 @@ MainWindow::MainWindow(QWidget *parent, QString boardName, QString cameraLocatio
 
     ui->actionEnable_ArmNN_Delegate->setEnabled(false);
     ui->actionTensorFlow_Lite->setEnabled(true);
-    ui->actionTensorflow_Lite_XNNPack_delegate->setEnabled(false);
+    ui->actionTensorflow_Lite_XNNPack_delegate->setEnabled(true);
 
     connect(this, SIGNAL(sendMatToDraw(cv::Mat)), this, SLOT(drawMatToView(cv::Mat)));
 