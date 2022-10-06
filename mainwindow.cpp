renesas-rz/rz-edge-ai-demo

commit e39d692039264ca930ada37d6930088c10ceb5cb

mainwindow: Use media sub directories for each mode
Example media is distributed in directories in the RFS for each
mode. We should open the file menu in the correct directory for
the running mode for the user.

Signed-off-by: Gareth Williams <gareth.williams.jx@renesas.com>

mainwindow.cpp
@@ -1009,6 +1009,7 @@ void MainWindow::on_actionLoad_File_triggered()
     QFileDialog dialog(this);
     QString mediaFileFilter;
     QString mediaFileName;
+    QString mediaDir;
 
     connect(this, SIGNAL(fileLoaded()), qeventLoop, SLOT(quit()));
 
@@ -1020,7 +1021,20 @@ void MainWindow::on_actionLoad_File_triggered()
 
     dialog.setFileMode(QFileDialog::AnyFile);
     dialog.setViewMode(QFileDialog::Detail);
-    dialog.setDirectory(MEDIA_DIRECTORY);
+
+    switch (demoMode)
+    {
+       case SB: mediaDir = MEDIA_DIRECTORY_SB;
+         break;
+       case OD: mediaDir = MEDIA_DIRECTORY_FD;
+         break;
+       case PE: mediaDir = MEDIA_DIRECTORY_PE;
+         break;
+       case FD: mediaDir = MEDIA_DIRECTORY_OD;
+         break;
+       default: mediaDir = MEDIA_DIRECTORY;
+    }
+    dialog.setDirectory(mediaDir);
 
     if (demoMode != SB)
         mediaFileFilter += VIDEO_FILE_FILTER;

mainwindow.h
@@ -38,6 +38,10 @@
 #define LABEL_PATH_OD "/opt/rz-edge-ai-demo/labels/mobilenet_ssd_v2_coco_quant_postprocess_labels.txt"
 #define LABEL_PATH_SB "/opt/rz-edge-ai-demo/labels/shoppingBasketDemo_labels.txt"
 #define MEDIA_DIRECTORY "/opt/rz-edge-ai-demo/media/"
+#define MEDIA_DIRECTORY_SB "/opt/rz-edge-ai-demo/media/shopping-basket/"
+#define MEDIA_DIRECTORY_FD "/opt/rz-edge-ai-demo/media/face-detection/"
+#define MEDIA_DIRECTORY_PE "/opt/rz-edge-ai-demo/media/pose-estimation/"
+#define MEDIA_DIRECTORY_OD "/opt/rz-edge-ai-demo/media/object-detection/"
 #define MODEL_DIRECTORY "/opt/rz-edge-ai-demo/models/"
 #define MODEL_PATH_OD "/opt/rz-edge-ai-demo/models/mobilenet_ssd_v2_coco_quant_postprocess.tflite"
 #define MODEL_PATH_SB "/opt/rz-edge-ai-demo/models/shoppingBasketDemo.tflite"

commit fa493e388c4ba7847b0c89dd66941e6ae0b3a036

ui: Correct default media directory assignments
The current mode and media directory should match for the file menu.

Fixes: e39d69203926

Signed-off-by: Gareth Williams <gareth.williams.jx@renesas.com>

mainwindow.cpp
@@ -1031,11 +1031,11 @@ void MainWindow::on_actionLoad_File_triggered()
     {
        case SB: mediaDir = MEDIA_DIRECTORY_SB;
          break;
-       case OD: mediaDir = MEDIA_DIRECTORY_FD;
+       case OD: mediaDir = MEDIA_DIRECTORY_OD;
          break;
        case PE: mediaDir = MEDIA_DIRECTORY_PE;
          break;
-       case FD: mediaDir = MEDIA_DIRECTORY_OD;
+       case FD: mediaDir = MEDIA_DIRECTORY_FD;
          break;
        default: mediaDir = MEDIA_DIRECTORY;
     }